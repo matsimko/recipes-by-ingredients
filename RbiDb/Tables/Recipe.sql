056c84228e22981afc8f618d62d45499b111617e
Matěj Šimko <mat3j.simko@gmail.com>
2023-03-23 10:28:56 +0100

    Remove hash, salt, username, and email from user table; add user id to recipe table

--- RbiDb/Tables/Recipe.sql
+++ RbiDb/Tables/Recipe.sql
@@ -3,5 +3,9 @@
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
 	[Name] NVARCHAR(200) NOT NULL, 
     [Description] NVARCHAR(MAX) NULL, 
-    [IsPublic] BIT NOT NULL DEFAULT 0
+    [IsPublic] BIT NOT NULL DEFAULT 0, 
+    [UserId] INT NULL,
+
+    CONSTRAINT [FK_Recipe_User] FOREIGN KEY ([UserId]) REFERENCES [User]([Id]),
+
 )
