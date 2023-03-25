c1304cd2242112606a72659efcefd391988c281e
Matěj Šimko <mat3j.simko@gmail.com>
2023-03-25 11:16:29 +0100

    Add entities.

---
+++ RbiData/Entities/Recipe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RbiData.Entities;
+
+public class Recipe
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsPublic { get; set; }
+    public User? User { get; set; }
+    public IEnumerable<UsedIngredient>? Ingredients { get; set; }
+    public IEnumerable<Tag>? Tags { get; set; }
+    public string? Description { get; set; }
+}
---
+++ RbiData/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RbiData.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+
+    //If a user is anonymous, their public recipes won't display their username to others
+    public string? IsAnonymous { get; set; }
+
+}
