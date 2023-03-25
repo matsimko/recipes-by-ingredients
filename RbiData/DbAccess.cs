de9fec74a66cc63b952bfa6e845d88f008f0374b
Matěj Šimko <mat3j.simko@gmail.com>
2023-03-25 11:15:46 +0100

    Remove db helper and add db access class which allows creation of db connections

---
+++ RbiData/DbAccess.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace RbiData;
+
+internal class DbAccess
+{
+    private readonly IConfiguration _config;
+
+    public DbAccess(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    //Use a connection when you need to run a custom transaction
+    public IDbConnection NewConnection(string connectionId = "Default")
+    {
+        return new SqlConnection(_config.GetConnectionString(connectionId));
+    }
+}
