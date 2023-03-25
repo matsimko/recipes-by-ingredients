b925ec32f5015f5d5440b8d22b0b2db11ce48ea1
Matěj Šimko <mat3j.simko@gmail.com>
2023-03-25 11:12:03 +0100

    Add association table for recipe and tag. Add amount unit column to used ingredient table.

---
+++ RbiDb/Tables/RecipeTag.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[RecipeTag]
+(
+    [RecipeId] INT NOT NULL, 
+    [TagId] INT NOT NULL, 
+
+    CONSTRAINT [PK_RecipeTag] PRIMARY KEY ([RecipeId], [TagId]),
+    CONSTRAINT [FK_RecipeTag_Recipe] FOREIGN KEY ([RecipeId]) REFERENCES [Recipe]([Id]), 
+    CONSTRAINT [FK_RecipeTag_Tag] FOREIGN KEY ([TagId]) REFERENCES [Tag]([Id])
+)
--- RbiDb/Tables/UsedIngredient.sql
+++ RbiDb/Tables/UsedIngredient.sql
@@ -3,6 +3,7 @@
 	[IngredientId] INT NOT NULL,
 	[RecipeId] INT NOT NULL,
     [Amount] DECIMAL(8, 2) NULL, 
+    [AmountUnit] NCHAR(10) NULL,
 
     CONSTRAINT [PK_UsedIngredient] PRIMARY KEY (IngredientId, RecipeId),
     CONSTRAINT [FK_UsedIngredient_Recipe] FOREIGN KEY (RecipeId) REFERENCES Recipe(Id), 
